a2d6fc799e8c3db11095a1ef1309e549809e2eb7
Kentaro Wada <www.kentaro.wada@gmail.com>
2016-06-24 02:29:22 +0900

    Add example for APC2016

---
+++ examples/apc2016/apc2016.py
@@ -0,0 +1,186 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import cPickle as pickle
+import os.path as osp
+
+import numpy as np
+import plyvel
+import scipy.ndimage as ndi
+import skimage.transform
+
+import fcn
+
+
+_this_dir = osp.dirname(osp.realpath(__file__))
+
+
+class APC2016Dataset(object):
+
+    target_names = np.array([
+        'no_object',
+        'barkely_hide_bones',
+        'cherokee_easy_tee_shirt',
+        'clorox_utility_brush',
+        'cloud_b_plush_bear',
+        'command_hooks',
+        'cool_shot_glue_sticks',
+        'crayola_24_ct',
+        'creativity_chenille_stems',
+        'dasani_water_bottle',
+        'dove_beauty_bar',
+        'dr_browns_bottle_brush',
+        'easter_turtle_sippy_cup',
+        'elmers_washable_no_run_school_glue',
+        'expo_dry_erase_board_eraser',
+        'fiskars_scissors_red',
+        'fitness_gear_3lb_dumbbell',
+        'folgers_classic_roast_coffee',
+        'hanes_tube_socks',
+        'i_am_a_bunny_book',
+        'jane_eyre_dvd',
+        'kleenex_paper_towels',
+        'kleenex_tissue_box',
+        'kyjen_squeakin_eggs_plush_puppies',
+        'laugh_out_loud_joke_book',
+        'oral_b_toothbrush_green',
+        'oral_b_toothbrush_red',
+        'peva_shower_curtain_liner',
+        'platinum_pets_dog_bowl',
+        'rawlings_baseball',
+        'rolodex_jumbo_pencil_cup',
+        'safety_first_outlet_plugs',
+        'scotch_bubble_mailer',
+        'scotch_duct_tape',
+        'soft_white_lightbulb',
+        'staples_index_cards',
+        'ticonderoga_12_pencils',
+        'up_glucose_bottle',
+        'womens_knit_gloves',
+        'woods_extension_cord',
+    ])
+    mean_bgr = np.array((104.00698793, 116.66876762, 122.67891434))
+
+    def __init__(self):
+        self.dataset_dir = osp.realpath(
+            osp.join(_this_dir, 'dataset/APC2016jsk'))
+        self.train, self.test = self.scrape()
+        self.db = plyvel.DB(osp.join(_this_dir, 'leveldb'),
+                            create_if_missing=True)
+
+    def scrape(self):
+        # train
+        train_txt = osp.join(self.dataset_dir, 'train.txt')
+        train_dirs = [osp.join(self.dataset_dir, 'all', dir_.strip())
+                      for dir_ in open(train_txt, 'r').readlines()]
+        train_data = self._scrape_dirs(train_dirs)
+        # test
+        test_txt = osp.join(self.dataset_dir, 'test.txt')
+        test_dirs = [osp.join(self.dataset_dir, 'all', dir_.strip())
+                     for dir_ in open(test_txt, 'r').readlines()]
+        test_data = self._scrape_dirs(test_dirs)
+        return train_data, test_data
+
+    def _scrape_dirs(self, dirs):
+        dataset = []
+        for dir_ in dirs:
+            img_file = osp.join(dir_, 'image.png')
+            mask_file = osp.join(dir_, 'mask.png')
+            label_file = osp.join(dir_, 'label.txt')
+            label_name = open(label_file, 'r').read().strip()
+            dataset.append((img_file, mask_file, label_name))
+        return dataset
+
+    def rgb_to_blob(self, rgb):
+        rgb = rgb.astype(np.float32)
+        blob = rgb[:, :, ::-1]  # RGB-> BGR
+        blob -= self.mean_bgr
+        blob = blob.transpose((2, 0, 1))
+        return blob
+
+    def blob_to_rgb(self, blob):
+        bgr = blob.transpose((1, 2, 0))
+        bgr += self.mean_bgr
+        rgb = bgr[:, :, ::-1]  # BGR -> RGB
+        rgb = rgb.astype(np.uint8)
+        return rgb
+
+    def transform_img(self, img, mask, train):
+        img_trans = img.copy()
+        if train:
+            # translate
+            height, width = img_trans.shape[:2]
+            translation = (int(0.1 * np.random.random() * height),
+                           int(0.1 * np.random.random() * width))
+            tform = skimage.transform.SimilarityTransform(
+                translation=translation)
+            img_trans = skimage.transform.warp(img_trans, tform, mode='edge',
+                                               preserve_range=True)
+            img_trans = img_trans.astype(np.uint8)
+        # apply mask
+        img_trans[mask == 0] = self.mean_bgr[::-1]
+        img_trans = fcn.util.apply_mask(
+            img_trans, mask, crop=True, fill_black=False)
+        # resize
+        # img_trans, _ = fcn.util.resize_with_min_hw(img_trans, min_hw=256)
+        img_trans = skimage.transform.resize(
+            img_trans, (224, 224), preserve_range=True).astype(np.uint8)
+        return img_trans
+
+    def load_datum(self, datum, train):
+        assert isinstance(datum, tuple)
+        assert len(datum) == 3
+        datum_id = '_'.join(datum)
+        inputs = self.db.get(datum_id)
+        if inputs is not None:
+            return pickle.loads(inputs)
+        img_file, mask_file, label_name = datum
+        img = ndi.imread(img_file, mode='RGB')
+        mask = ndi.imread(mask_file, mode='L')
+        # resize mask image
+        if img.shape[:2] != mask.shape[:2]:
+            print('WARNING: img and mask must have same shape. '
+                  'Resizing mask {} to img {}.'
+                  .format(img.shape[:2], mask.shape))
+            mask = skimage.transform.resize(
+                mask, img.shape[:2], preserve_range=True).astype(np.uint8)
+        label_id = np.where(self.target_names == label_name)[0][0]
+        img_trans = self.transform_img(img, mask, train=train)
+        blob = self.rgb_to_blob(img_trans)
+        inputs = (blob, label_id)
+        self.db.put(datum_id, pickle.dumps(inputs))
+        return inputs
+
+    def next_datum(self, train, index=None):
+        data = self.train if train else self.test
+        if index is None:
+            index = np.random.randint(0, len(data))
+        return self.load_datum(data[index], train=train)
+
+    def next_batch(self, batch_size, type, type_indices=None):
+        assert type in ('train', 'test')
+        if type_indices is None:
+            type_indices = [None] * batch_size
+        x, t = [], []
+        for index in type_indices:
+            blob, target = self.next_datum(train=type == 'train', index=index)
+            x.append(blob)
+            t.append(target)
+        x = np.array(x, dtype=np.float32)
+        t = np.array(t, dtype=np.int32)
+        return x, t
+
+
+if __name__ == '__main__':
+    from skimage.color import label2rgb
+    import matplotlib.pyplot as plt
+    dataset = APC2016Dataset()
+    for datum in dataset.train:
+        img_file, mask_file, label_name = datum
+        img = ndi.imread(img_file, mode='RGB')
+        mask = ndi.imread(mask_file, mode='L')
+        label = (mask != 0).astype(np.int32)
+        label_viz = label2rgb(label, img, bg_label=0)
+        plt.imshow(label_viz)
+        plt.show()
---
+++ examples/apc2016/train.py
@@ -0,0 +1,169 @@
+#!/usr/bin/env python
+
+from __future__ import division
+from __future__ import print_function
+
+import argparse
+import os
+import os.path as osp
+
+from chainer import cuda
+import chainer.optimizers as O
+import chainer.serializers as S
+from chainer import Variable
+import tqdm
+
+import apc2016
+import fcn
+import fcn.models
+import fcn.util
+from vgg16 import VGG16
+
+
+class Trainer(object):
+
+    def __init__(self, gpu, model, optimizer, dataset, save_dir=None,
+                 test_interval=1000, snapshot_interval=4000,
+                 max_iteration=100000, batch_size=20):
+        self.gpu = gpu
+        self.model = model
+        self.optimizer = optimizer
+        self.dataset = dataset
+
+        self.save_dir = 'snapshot' if save_dir is None else save_dir
+        if not osp.exists(self.save_dir):
+            os.makedirs(self.save_dir)
+
+        self.test_interval = test_interval
+        self.snapshot_interval = snapshot_interval
+        self.max_iteration = max_iteration
+        self.batch_size = batch_size
+
+        self.log_file = open(osp.join(self.save_dir, 'log.csv'), 'a')
+        print('i_iter,type,loss,acc', file=self.log_file)
+        self.csv_templ = '{i_iter},{type},{loss},{acc}'
+        self.display_templ = '{i_iter}: type={type}, loss={loss}, acc={acc}'
+
+    def __del__(self):
+        self.log_file.close()
+
+    def test(self):
+        n_data = len(self.dataset.test)
+        sum_loss, sum_acc = 0, 0
+        desc = '{0}: test iteration'.format(self.i_iter)
+        pbar = tqdm.tqdm(total=n_data, ncols=80, desc=desc)
+        for index_start in xrange(0, n_data, self.batch_size):
+            pbar.update(self.batch_size)
+            index_stop = min(index_start + self.batch_size, n_data)
+            type_indices = range(index_start, index_stop)
+            loss, acc, actual_batch_size = \
+                self.iterate_once(type='test', type_indices=type_indices)
+            sum_loss += (loss * actual_batch_size)
+            sum_acc += (acc * actual_batch_size)
+        pbar.close()
+        mean_loss = sum_loss / n_data
+        mean_acc = sum_acc / n_data
+        return mean_loss, mean_acc
+
+    def iterate_once(self, type, type_indices=None):
+        assert type in ('train', 'test')
+        x, t = self.dataset.next_batch(batch_size=self.batch_size, type=type,
+                                       type_indices=type_indices)
+        if self.gpu != -1:
+            x = cuda.to_gpu(x, self.gpu)
+        x = Variable(x, volatile=not self.model.train)
+        if self.gpu != -1:
+            t = cuda.to_gpu(t, self.gpu)
+        t = Variable(t, volatile=not self.model.train)
+        if self.model.train:
+            self.optimizer.zero_grads()
+            self.optimizer.update(self.model, x, t)
+        else:
+            self.model(x, t)
+        loss = float(cuda.to_cpu(self.model.loss.data))
+        acc = float(cuda.to_cpu(self.model.acc.data))
+        actual_batch_size = len(x.data)  # sometimes != self.batch_size
+        return loss, acc, actual_batch_size
+
+    def snapshot(self):
+        S.save_hdf5(osp.join(self.save_dir,
+                             'vgg16_{0}.chainermodel'.format(self.i_iter)),
+                    self.model)
+        S.save_hdf5(osp.join(self.save_dir,
+                             'vgg16_optimizer_{0}.h5'.format(self.i_iter)),
+                    self.optimizer)
+
+    def run(self, iter_start):
+        for i_iter in xrange(iter_start, self.max_iteration, self.batch_size):
+            self.i_iter = i_iter
+
+            # snapshot
+            if self.i_iter % self.snapshot_interval == 0:
+                self.snapshot()
+
+            # test
+            if self.i_iter % self.test_interval == 0:
+                self.model.train = False
+                loss, acc = self.test()
+                log = dict(i_iter=self.i_iter, type='test', loss=loss, acc=acc)
+                print(self.display_templ.format(**log))
+                print(self.csv_templ.format(**log), file=self.log_file)
+
+            # train
+            self.model.train = True
+            loss, acc, _ = self.iterate_once(type='train')
+            log = dict(i_iter=self.i_iter, type='train', loss=loss, acc=acc)
+            print(self.display_templ.format(**log))
+            print(self.csv_templ.format(**log), file=self.log_file)
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--gpu', type=int, default=0)
+    parser.add_argument(
+        '--resume', nargs=3,
+        metavar=('ITER_START', 'CHAINERMODEL', 'OPTIMIZER_STATE'),
+        default=('0', None, None))
+    parser.add_argument('--max-iter', type=int, default=100000)
+    args = parser.parse_args()
+
+    gpu = args.gpu
+    iter_start, chainermodel, optimizer_h5 = args.resume
+    iter_start = int(iter_start)
+    max_iter = args.max_iter
+
+    # dataset
+    dataset = apc2016.APC2016Dataset()
+    n_class = len(dataset.target_names)
+
+    # model
+    model = VGG16(n_class=n_class)
+    if chainermodel is None:
+        # copy weights from pretrained model
+        model_path = fcn.setup.download_vgg16_chainermodel()
+        vgg16_orig = fcn.models.VGG16()
+        S.load_hdf5(model_path, vgg16_orig)
+        fcn.util.copy_chainermodel(vgg16_orig, model)
+    else:
+        S.load_hdf5(chainermodel, model)
+    if gpu != -1:
+        model.to_gpu(gpu)
+
+    # optimizer
+    optimizer = O.Adam()
+    optimizer.setup(model)
+    if optimizer_h5 is not None:
+        S.load_hdf5(optimizer_h5, optimizer)
+
+    trainer = Trainer(
+        gpu=gpu,
+        model=model,
+        optimizer=optimizer,
+        dataset=dataset,
+        max_iteration=max_iter,
+    )
+    trainer.run(iter_start)
+
+
+if __name__ == '__main__':
+    main()
